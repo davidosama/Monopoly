8bfa302631b6778ace0c21cb861f217495a25829
David Osama <david_u_riad@hotmail.com>
2017-10-09 01:27:07 +0200

    add board and city classes

---
+++ src/monopoly/Board.java
@@ -0,0 +1,9 @@
+package monopoly;
+
+
+public class Board {
+    //fixed number of cities, railroads & companies
+    normalCity[] normalCities = new normalCity[22];
+    RailRoad[] railRoads = new RailRoad[4];
+    Company[] companies = new Company[2];
+}
