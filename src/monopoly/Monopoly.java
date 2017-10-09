a743f3ce9b63694034d465764cceeaa7ad24fbc7
hesham-akmal <hesham.akmal@gmail.com>
2017-10-09 21:40:42 +0200

    window created

--- src/monopoly/Monopoly.java
+++ src/monopoly/Monopoly.java
@@ -1,9 +1,20 @@
 package monopoly;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class Monopoly {
 
     public static void main(String[] args) {
 
+        //Creating window
+        JWindow window = new JWindow();
+
+        //Sets the window position to the screen center
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        window.setLocation(dim.width / 2 - window.getSize().width / 2, dim.height / 2 - window.getSize().height / 2);
+
+        //Show window
+        window.setVisible(true);
     }
-    
 }
