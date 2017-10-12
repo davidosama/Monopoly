b8c7d77e9bcb0d40ba068ed485fa88d384b6d644
David Osama <david_u_riad@hotmail.com>
2017-10-12 23:38:55 +0200

    Revert "Merge branch 'test'"
    This reverts commit 6069579b6ad373d20627cb4ac2a4c3da05b53a02, reversing
    changes made to a0be96de54b72f900f0e6ed15b8dcbfd1d7bac87.

--- src/monopoly/Player.java
+++ src/monopoly/Player.java
@@ -5,15 +5,71 @@
  */
 package monopoly;
 
-/**
- *
- * @author Kord
- */
-public class player {
-   // i assumed you would create this class then we will need to integrate moveCar function into it
-    //this class is dummy one you can add props into it
-    
-   
-    
-    
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class Player {
+
+    //Player number
+    public int num;
+
+    //Player label (pic)
+    public JLabel label;
+
+    //City Number
+    public int currentCity;
+
+    //Array of all the players
+    public static ArrayList<Player> playersList = new ArrayList<>();
+
+    public static int playersCount = 0;
+
+    private static int Turn = 0;
+
+    public Player() {
+
+        //initialize current city to zero
+        currentCity = 0;
+
+        //initialize player number to the playersCount and increment
+        num = ++playersCount;
+
+        //add player in allPlayers List
+        playersList.add(this);
+
+        //create a label for the player
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                label = new JLabel();
+                label.setIcon(new javax.swing.ImageIcon(getClass().getResource("/drawables/CarLeft" + num + ".png")));
+                label.setBounds(620, 620, 45, 50);
+                Constants.gameWindow.getJlabel1().add(label);
+                Constants.gameWindow.getJlabel1().validate();
+                Constants.gameWindow.getJlabel1().repaint();
+            }
+        });
+
+        //debugPrintPlayer();
+    }
+
+    public static void MoveTurn() {
+
+        Turn = (Turn + 1) % playersCount;
+
+    }
+
+    public static Player getPlayer() {
+        return playersList.get(Turn);
+    }
+
+    private void debugPrintPlayer() {
+
+        System.out.print("\n\nplayer Number: " + num
+                + "\nplayers Count: " + playersCount
+                + "\nplayer Turn: " + Turn
+                + "\ncurrentCity: " + currentCity
+        );
+    }
 }
