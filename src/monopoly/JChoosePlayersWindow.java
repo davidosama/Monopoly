a5f027c1350b1955338bbae6a9c8be08df16ebd2
Ahmed1337 <ahmedhamed523@gmail.com>
2017-10-23 17:42:47 +0200

    Merge remote-tracking branch 'origin/7amed'

--- src/monopoly/JChoosePlayersWindow.java
+++ src/monopoly/JChoosePlayersWindow.java
@@ -90,20 +90,43 @@ public class JChoosePlayersWindow extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        Monopoly.CreatePlayers(2);
+        startGameWindow(2);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        Monopoly.CreatePlayers(3);
+        startGameWindow(3);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        Monopoly.CreatePlayers(4);
+        startGameWindow(4);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        Monopoly.CreatePlayers(5);
+        startGameWindow(5);
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    public void createPlayers(int number) {
+
+        //Creates and add players into the array
+        for (int i = 0; i < number; i++) {
+            Player.playersList.add(new Player());
+        }
+
+    }
+
+    private void startGameWindow(int playersCount) {
+
+        Constants.gameWindow = new JGameWindow();
+        
+        createPlayers(playersCount);
+        //Hide ChoosePlayersWindow
+        Constants.choosePlayersWindow.dispose();
+
+        Constants.gameWindow.setLocationRelativeTo(null);
+
+        //Start gameWindow
+        Constants.gameWindow.setVisible(true);
+    }
 
     /**
      * @param args the command line arguments
@@ -136,7 +159,14 @@ public class JChoosePlayersWindow extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new JChoosePlayersWindow().setVisible(true);
+                Board board = new Board();
+
+                Constants.choosePlayersWindow = new JChoosePlayersWindow();
+                Constants.choosePlayersWindow.setLocationRelativeTo(null);
+                Constants.choosePlayersWindow.setVisible(true);
+
+                
+
             }
         });
     }
